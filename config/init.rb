c2796a8c203f154996afa326181b17b82014f656
Sylvain Joyeux <sylvain.joyeux@m4x.org>
2013-02-15 13:52:19 +0100

    add the stanza to enable transformer support, but commented out

--- config/init.rb
+++ config/init.rb
@@ -18,6 +18,9 @@ Roby.app.using 'syskit'
 require 'roby/schedulers/temporal'
 Roby.scheduler = Roby::Schedulers::Temporal.new
 
+## Uncomment to enable automatic transformer configuration support
+# Syskit.conf.transformer_enabled = true
+
 ##############################
 # Sets some configuration options
 #
